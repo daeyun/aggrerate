d7881e1dbc016021e54d207821b7001e0c47c9a7
mfhoffm2 <mfhoffm2@illinois.edu>
2013-03-09 21:26:26 -0600

    Added product review form

--- aggrerate/views.py
+++ aggrerate/views.py
@@ -17,3 +17,22 @@ def reviews():
 def login():
     return render_template('login.html')
 
+@app.route('/product/')
+@app.route('/product/<productId>')
+def enterReview(productId=None):
+	return render_template('enterReview.html', productId=productId)
+
+@app.route('/postReview/', methods=['POST'])
+def postReview():
+	db = MySQLdb.connect(host='ec2-174-129-96-104.compute-1.amazonaws.com',
+		user='matt',
+		passwd='matt',
+		db='aggrerate')
+	cur = db.cursor()
+	dtstr = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+	# dtstr = str(dt.year)+"-"+str(dt.month)+"-"+str(dt.day)+" "+str(dt.hour)+":"+str(dt.minute)+":"+str(dt.second)
+	query = 'INSERT INTO reviews VALUES (%s, "%s", %s, %s, "%s")' % (8, dtstr, request.form['score'], request.form['productId'], request.form['reviewText'])
+	print query
+	cur.execute(query)
+	db.commit()
+	return render_template('successfulReview.html')
