438ab6a7588258f8d9e89c7099d38e1979210077
Jeff Wheeler <jeffwheeler@gmail.com>
2013-03-09 21:12:55 -0600

    Split up scraper

--- aggrerate/scraper/site_scrapers.py
+++ aggrerate/scraper/site_scrapers.py
@@ -1,46 +1,4 @@
-import urllib2
-from urlparse import urlparse
-
-from bs4 import BeautifulSoup
-import MySQLdb as mdb
-
-class ReviewScraper(object):
-    scrapers = []
-
-    def __init__(self, url):
-        self.url   = url
-        self.soup  = None
-        self.score = None
-
-    @classmethod
-    def from_url(cls, url):
-        domain = urlparse(url).netloc
-        for scraper in cls.scrapers:
-            if scraper.matches_url(url):
-                return scraper(url)
-
-    def download_page(self):
-        if self.soup is not None:
-            return
-
-        try:
-            p = urllib2.urlopen(self.url, None)
-
-            # We use Python's builtin HTML parser instead of lxml because lxml
-            # seems to fail partway through parsing some pages (because of
-            # errors? size? not sure)
-            self.soup = BeautifulSoup(p.read(), "html.parser")
-        except:
-            print "Failed to download page '%s'" % self.url
-
-    def parse_page(self):
-        raise NotImplementedError
-
-    def __str__(self):
-        return '%s(%s)' % (self.__class__.site_name, self.score)
-
-def register_scraper(scraper):
-    ReviewScraper.scrapers.append(scraper)
+from aggrerate.scraper import ReviewScraper, register_scraper
 
 @register_scraper
 class VergeScraper(ReviewScraper):
@@ -118,17 +76,3 @@ class PCMagScraper(ReviewScraper):
         except:
             print "Unable to find score on given page"
 
-if __name__ == "__main__":
-    urls = [
-        "http://www.theverge.com/2012/11/2/3589280/google-nexus-4-review",
-        "http://www.theverge.com/2012/11/1/3584486/nokia-lumia-920-review",
-        "http://reviews.cnet.com/samsung-galaxy-s3-review/",
-        "http://gdgt.com/apple/iphone/5/",
-        "http://www.pcmag.com/article2/0,2817,2414819,00.asp"
-    ]
-
-    for url in urls:
-        scraper = ReviewScraper.from_url(url)
-        if scraper:
-            scraper.parse_page()
-            print scraper
