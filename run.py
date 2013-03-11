4e146d9be0a00d687a8f403115177d992e1fe6cc
Jeff Wheeler <jeffwheeler@gmail.com>
2013-03-11 01:19:24 -0500

    Allow run.py to use local configuration that sets debug=True

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from aggrerate import app
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        from local_config import run_params
+    except ImportError:
+        run_params = {}
+    app.run(**run_params)
