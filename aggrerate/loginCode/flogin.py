6f6cf242debd90804c3a13535b39eba87f669c42
Matt Hoffman <matt@domU-12-31-39-0A-D4-68.compute-1.internal>
2013-03-14 05:24:33 +0000

    New user handling

---
+++ aggrerate/loginCode/flogin.py
@@ -0,0 +1,24 @@
+from flask.ext import login
+from aggrerate import util
+
+class User(login.UserMixin):
+    @classmethod
+    def get(cls, userid):
+        data = util.get_userdata(username = userid)
+        
+
+        return User(data, userid=="matt")
+
+    def __init__(self, data, admin):
+        self.data = data
+        self.admin = admin
+
+    def get_username(self):
+        return self.data["username"]
+
+    def is_admin(self):
+        return self.admin
+
+    def get_id(self):
+        return self.data["username"]
+
